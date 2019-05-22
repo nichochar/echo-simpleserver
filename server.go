9f1df64f1e61571d1594a8e824a52188a14d0a60
Nicholas Charriere <nicholas.charriere@getcruise.com>
2019-05-22 15:53:57 -0700

    Add templates, add form to create a user

--- server.go
+++ server.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"html/template"
+	"io"
 	"net/http"
 	"strconv"
 	"strings"
@@ -15,8 +17,18 @@ type User struct {
 	Age  int
 }
 
-func (u User) String() string {
+func (u *User) String() string {
 	return fmt.Sprintf("Name: %s || Age: %d\n", u.Name, u.Age)
+}
+
+// Template allows us to implement the echo.Renderer interface
+type Template struct {
+	templates *template.Template // use stdlib html/template
+}
+
+// Render implements the actual echo.Renderer interface on a Template object
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 // DB is an in memory database: map of user names to their structs
@@ -40,7 +52,7 @@ func getUser(c echo.Context) error {
 	return c.String(http.StatusOK, user.String())
 }
 
-func putUser(c echo.Context) error {
+func createUser(c echo.Context) error {
 	name := c.FormValue("name")
 	ageRaw := c.FormValue("age")
 	age, err := strconv.Atoi(ageRaw)
@@ -56,12 +68,18 @@ func putUser(c echo.Context) error {
 	return c.String(http.StatusOK, "Added "+name)
 }
 
+func newUser(c echo.Context) error {
+	return c.Render(http.StatusOK, "new_user", nil)
+}
+
 func main() {
 	e := echo.New()
 	e.Static("/static", "static")
+	e.Renderer = &Template{templates: template.Must(template.ParseGlob("templates/*.html"))}
 	e.GET("/", getIndex)
 	e.GET("/users/:name", getUser)
-	e.PUT("/users", putUser)
+	e.GET("/users/new", newUser)
+	e.POST("/users", createUser)
 
 	e.Logger.Fatal(e.Start(":1323"))
 }
